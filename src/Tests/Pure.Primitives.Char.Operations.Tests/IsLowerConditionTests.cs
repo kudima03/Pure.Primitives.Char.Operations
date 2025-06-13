a5190dbdae98945b20409f3b910f50b405f56960
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-13 12:14:22 +0300

    Implemented tests for IsLowerCondition.

---
+++ src/Tests/Pure.Primitives.Char.Operations.Tests/IsLowerConditionTests.cs
@@ -0,0 +1,69 @@
+﻿using Pure.Primitives.Abstractions.Bool;
+
+namespace Pure.Primitives.Char.Operations.Tests;
+
+public sealed record IsLowerConditionTests
+{
+    [Fact]
+    public void TakesPositiveResultOnLowers()
+    {
+        IBool equality = new IsLowerCondition(
+            new Char('a'),
+            new Char('s'),
+            new Char('d'),
+            new Char('f'),
+            new Char('g'),
+            new Char('h'));
+
+        Assert.True(equality.BoolValue);
+    }
+
+    [Fact]
+    public void TakesFalseResultOnAllLowersOneNot()
+    {
+        IBool equality = new IsLowerCondition(
+            new Char('a'),
+            new Char('s'),
+            new Char('d'),
+            new Char('S'),
+            new Char('f'),
+            new Char('g'),
+            new Char('h'));
+
+        Assert.False(equality.BoolValue);
+    }
+
+    [Fact]
+    public void TakesFalseResultOnUpper()
+    {
+        IBool equality = new IsLowerCondition(new Char('A'));
+
+        Assert.False(equality.BoolValue);
+    }
+
+    [Fact]
+    public void ProduceTrueOnSingleElementInCollection()
+    {
+        IBool equality = new IsLowerCondition(new Char('a'));
+        Assert.True(equality.BoolValue);
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnEmptyArguments()
+    {
+        IBool equality = new IsLowerCondition();
+        Assert.Throws<ArgumentException>(() => equality.BoolValue);
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new IsLowerCondition(new Char('A')).GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new IsLowerCondition(new Char('A')).ToString());
+    }
+}
