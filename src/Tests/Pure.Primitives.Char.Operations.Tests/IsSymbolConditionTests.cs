91220cfbe1b12df9b0f1e0ee53868e4f8b7a4aea
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-13 12:39:16 +0300

    Implemented tests for IsSymbolCondition.

---
+++ src/Tests/Pure.Primitives.Char.Operations.Tests/IsSymbolConditionTests.cs
@@ -0,0 +1,71 @@
+﻿using Pure.Primitives.Abstractions.Bool;
+
+namespace Pure.Primitives.Char.Operations.Tests;
+
+public sealed record IsSymbolConditionTests
+{
+    private readonly IEnumerable<char> _symbolChars = new[]
+    {
+        // Currency symbols
+        '$', '€', '£', '¥', '¢',
+
+        // Mathematical symbols
+        '+', '=', '×', '÷', '<', '>', '±',
+
+        // Modifier and other symbols
+        '^', '~', '|',
+
+        // Special symbols
+        '©', '®', '™', '✓', '✗'
+    };
+
+    [Fact]
+    public void TakesPositiveResultOnSymbols()
+    {
+        IBool equality = new IsSymbolCondition(_symbolChars.Select(x => new Char(x)));
+
+        Assert.True(equality.BoolValue);
+    }
+
+    [Fact]
+    public void TakesFalseResultOnAllSymbolsOneNot()
+    {
+        IBool equality = new IsSymbolCondition(_symbolChars.Select(x => new Char(x)).Append(new Char('A')));
+
+        Assert.False(equality.BoolValue);
+    }
+
+    [Fact]
+    public void TakesFalseResultOnNonSeparator()
+    {
+        IBool equality = new IsSymbolCondition(new Char('A'));
+
+        Assert.False(equality.BoolValue);
+    }
+
+    [Fact]
+    public void ProduceTrueOnSingleElementInCollection()
+    {
+        IBool equality = new IsSymbolCondition(_symbolChars.Select(x => new Char(x)).First());
+        Assert.True(equality.BoolValue);
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnEmptyArguments()
+    {
+        IBool equality = new IsSymbolCondition();
+        Assert.Throws<ArgumentException>(() => equality.BoolValue);
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new IsSymbolCondition(new Char('A')).GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new IsSymbolCondition(new Char('A')).ToString());
+    }
+}
